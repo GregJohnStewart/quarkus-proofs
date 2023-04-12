011452052c250164f35086b13b57f3fad620d411
GregJohnStewart <contact@gjstewart.net>
2023-04-12 13:36:10 -0400

    found the issue

--- 32552-multi-stor-proc-poc/src/test/resources/db/importScript.sql
+++ 32552-multi-stor-proc-poc/src/test/resources/db/importScript.sql
@@ -6,18 +6,50 @@
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
 
-CREATE TABLE foo (
-    id int
+
+CREATE TABLE foo
+(
+	id int
 );
 
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
 
-CREATE PROCEDURE testOne
-    AS BEGIN
-SELECT * FROM foo;
-SELECT * FROM foo;
+CREATE PROCEDURE testOne(
+	@input VARCHAR(100)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	BEGIN TRY
+	    BEGIN TRANSACTION -- having this transaction causes issues
+		SELECT * FROM foo;
+
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+	    ROLLBACK TRANSACTION
+		SELECT * FROM foo;
+
+	END CATCH
+
 END;
 
-CREATE PROCEDURE testTwo
-    AS BEGIN
-SELECT * FROM foo;
-END;
+	CREATE PROCEDURE testTwo(
+		@input VARCHAR(100)
+	)
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+
+		BEGIN TRY
+			SELECT * FROM foo;
+
+		END TRY
+		BEGIN CATCH
+			SELECT * FROM foo;
+
+		END CATCH
+
+	END;
