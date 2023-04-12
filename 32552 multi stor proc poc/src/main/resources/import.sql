76394e331cf77aaa059a1d977786bac0455be2d4
Greg <gstewart@redhat.com>
2023-04-12 09:58:39 -0400

    Added more project setup

--- 32552 multi stor proc poc/src/main/resources/import.sql
+++ 32552 multi stor proc poc/src/main/resources/import.sql
@@ -1,5 +1,22 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
 -- This file allow to write SQL commands that will be emitted in test and dev.
 -- The commands are commented as their support depends of the database
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
+
+CREATE TABLE foo (
+    id int
+);
+
+
+CREATE PROCEDURE testOne
+    AS BEGIN
+SELECT * FROM foo;
+END;
+
+CREATE PROCEDURE testTwo
+    AS BEGIN
+SELECT * FROM foo;
+END;
