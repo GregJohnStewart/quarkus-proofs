aaab4a13b18fbbf33f29d597f82362eeb85b7b33
Greg Stewart <contact@gjstewart.net>
2023-04-12 07:23:09 -0700

    Update GreetingResourceTest.java

--- 32552 multi stor proc poc/src/test/java/org/acme/GreetingResourceTest.java
+++ 32552 multi stor proc poc/src/test/java/org/acme/GreetingResourceTest.java
@@ -17,5 +17,52 @@ public class GreetingResourceTest {
              .statusCode(200)
              .body(is("Hello from RESTEasy Reactive"));
     }
+    @Inject
+	DataSource ds;
+	
+	@Test
+	public void test() throws SQLException {
+		log.info("Successfully ran test setup");
+		
+		try(Connection db = ds.getConnection()){
+			try(
+					CallableStatement stmt = db.prepareCall("SELECT DB_NAME();");
+			){
+				stmt.execute();
+				ResultSet dbResult = stmt.getResultSet();
+				dbResult.next();
+				log.info("Database used: {}", dbResult.getString(1));
+			}
+		
+			try(
+					CallableStatement tstmt = db.prepareCall("SELECT * FROM master.INFORMATION_SCHEMA.TABLES;");
+			){
+				tstmt.execute();
+				
+				ResultSet tablesResult = tstmt.getResultSet();
+				ResultSetMetaData rsm = tablesResult.getMetaData();
+				
+				List<String> cols = new ArrayList<>();
+				for(int i = 1; i < rsm.getColumnCount(); i++) {
+					cols.add(rsm.getColumnName(i));
+				}
+				log.info("Columns: {}", cols);
+				
+				
+				while(tablesResult.next()) {
+					Map<String, Object> results = new HashMap<>();
+					
+					for(String curCol : cols) {
+						results.put(
+								curCol, 
+								tablesResult.getObject(curCol)
+								);
+					}
+					
+					log.info("Table: {}", results);
+				}
+			}
+		}
+	}
 
 }
