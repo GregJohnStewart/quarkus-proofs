763fb1c17035dad9badf0dfa6e3e39706d4d6447
GregJohnStewart <contact@gjstewart.net>
2023-07-04 09:23:32 -0400

    Added everything for uri builder tests

---
+++ uriBuilder bug/uri-bug-proof-classic/src/test/java/org/acme/UriBuilderTest.java
@@ -0,0 +1,38 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.UriBuilder;
+import org.apache.http.client.utils.URIBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+public class UriBuilderTest {
+    
+    private static final String BASE_URL = "http://localhost:6851/some/endpoint";
+    private static final String REDIRECT_URL_PARAM_NAME = "redirect_url";
+    private static final String REDIRECT_URL = "http://localhost:3185/some/other/endpoint?extra=fun&more=excitement";
+    
+    //NOTE:: this was available without extra dependencies
+//    private static final Logger LOG = Logger.getLogger(UriBuilderTest.class.getName());
+    
+    @Test
+    public void testUriBuilder() throws URISyntaxException {
+        UriBuilder uriBuilder = UriBuilder.fromUri(BASE_URL);
+        uriBuilder.queryParam(REDIRECT_URL_PARAM_NAME, REDIRECT_URL);
+        
+        String result = uriBuilder.build().toString();
+        
+        System.out.println("Uri builder: " + uriBuilder.toString());
+        System.out.println("Uri builder class: " + uriBuilder.getClass().toString());
+        
+        assertEquals(
+            new URIBuilder(BASE_URL).setParameter(REDIRECT_URL_PARAM_NAME, REDIRECT_URL).build().toString(),
+            result
+        );
+    }
+
+}
---
+++ uriBuilder bug/uri-bug-proof-reactive/src/test/java/org/acme/UriBuilderTest.java
@@ -0,0 +1,40 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.UriBuilder;
+import org.apache.http.client.utils.URIBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+public class UriBuilderTest {
+    
+    private static final String BASE_URL = "http://localhost:6851/some/endpoint";
+    private static final String REDIRECT_URL_PARAM_NAME = "redirect_url";
+    private static final String REDIRECT_URL = "http://localhost:3185/some/other/endpoint?extra=fun&more=excitement";
+    
+    private static final Logger LOG = Logger.getLogger(UriBuilderTest.class.getName());
+    
+    @Test
+    public void testUriBuilder() throws URISyntaxException {
+        UriBuilder uriBuilder = UriBuilder.fromUri(BASE_URL);
+        uriBuilder.queryParam(REDIRECT_URL_PARAM_NAME, REDIRECT_URL);
+        
+        String result = uriBuilder.build().toString();
+        
+        LOG.log(Level.INFO, "Uri builder: {0}", uriBuilder.toString());
+        LOG.log(Level.INFO, "Uri builder class: {0}", uriBuilder.getClass().toString());
+        
+        assertEquals(
+            new URIBuilder(BASE_URL).setParameter(REDIRECT_URL_PARAM_NAME, REDIRECT_URL).build().toString(),
+            result
+        );
+    }
+
+}
