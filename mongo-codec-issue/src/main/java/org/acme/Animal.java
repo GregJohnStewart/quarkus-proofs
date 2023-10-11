271a431f02424f30570bd851fbf8f864db38eea3
GregJohnStewart <contact@gjstewart.net>
2023-10-10 23:33:51 -0400

    Added bson discriminator

--- mongo-codec-issue/src/main/java/org/acme/Animal.java
+++ mongo-codec-issue/src/main/java/org/acme/Animal.java
@@ -1,7 +1,9 @@
 package org.acme;
 
+import org.bson.codecs.pojo.annotations.BsonDiscriminator;
 import org.bson.types.ObjectId;
 
+@BsonDiscriminator
 public abstract class Animal<T> {
 	public ObjectId id;
 	public T value;
--- mongo-codec-issue/src/main/java/org/acme/Cat.java
+++ mongo-codec-issue/src/main/java/org/acme/Cat.java
@@ -1,5 +1,8 @@
 package org.acme;
 
+import org.bson.codecs.pojo.annotations.BsonDiscriminator;
+
+@BsonDiscriminator
 public class Cat extends Animal<Integer> {
 	public Cat(int value){
 		this.value = value;
