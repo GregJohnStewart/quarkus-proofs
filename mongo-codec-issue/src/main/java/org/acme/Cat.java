c77703925acae2c1892abec0250878ffbce2aa39
GregJohnStewart <contact@gjstewart.net>
2023-10-18 18:26:09 -0400

    Added constructor fix

--- mongo-codec-issue/src/main/java/org/acme/Cat.java
+++ mongo-codec-issue/src/main/java/org/acme/Cat.java
@@ -4,6 +4,10 @@ import org.bson.codecs.pojo.annotations.BsonDiscriminator;
 
 @BsonDiscriminator
 public class Cat extends Animal<Integer> {
+	public Cat(){
+		this.value = 0;
+	}
+	
 	public Cat(int value){
 		this.value = value;
 	}
