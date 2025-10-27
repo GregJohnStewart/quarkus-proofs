7f50256eedbe6e35da312b60b8b9410decbc9d5f
GregJohnStewart <contact@gjstewart.net>
2025-10-27 17:23:36 -0400

    Finished poc

--- csrf-playground/src/main/java/org/acme/GreetingResource.java
+++ csrf-playground/src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,41 @@
 package org.acme;
 
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
-    }
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "Hello from Quarkus REST";
+	}
+	
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloPost() {
+		return "Hello from Quarkus REST";
+	}
+	@DELETE
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloDelete() {
+		return "Hello from Quarkus REST";
+	}
+	@PUT
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloPut() {
+		return "Hello from Quarkus REST";
+	}
+	@PATCH
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloPatch() {
+		return "Hello from Quarkus REST";
+	}
 }
